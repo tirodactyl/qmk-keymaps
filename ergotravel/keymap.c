f14ee823348977448e10fe99470401ee5acd8385
Tirso Peguero <tito.peguero@gmail.com>
2022-05-02 16:47:18 -0700

    Update tap mods to modern QMK syntax
    Updated to new format for tap mods to work with latest QMK.

--- ergotravel/keymap.c
+++ ergotravel/keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------+--------+--------+--------+--------+--------+--------.        ,--------+--------+--------+--------+--------+--------+--------.
      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______,          _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
-     F(2),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______,          _______, KC_H,    KC_J,    KC_K,    KC_L,    F(1),    F(3),
+LCTL_T(KC_ESC), KC_A,  KC_S,    KC_D,    KC_F,    KC_G,    _______,          _______, KC_H,    KC_J,    KC_K,    KC_L, LT(_VIM, KC_SCOLON), RCTL_T(KC_ENT),
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LALT,          KC_RGUI, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
@@ -176,14 +176,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-const uint16_t PROGMEM fn_actions[] = {
+// OLD TAP MODS
+// const uint16_t PROGMEM fn_actions[] = {
   // Layer switching
-  [1] = ACTION_LAYER_TAP_KEY(_VIM, KC_SCOLON),
+  // [1] = ACTION_LAYER_TAP_KEY(_VIM, KC_SCOLON),
 
   // Modifiers
-  [2] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),
-  [3] = ACTION_MODS_TAP_KEY(MOD_RCTL, KC_ENT),
-};
+  // [2] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),
+  // [3] = ACTION_MODS_TAP_KEY(MOD_RCTL, KC_ENT),
+// };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
